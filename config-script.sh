b32737e93cf5e63a620c7865f4a004e77526b63a
Thanh Tam <thanhtam@pop-os.localdomain>
2021-10-05 00:57:20 +0700

    Add nvim folder and symlink script

--- config-script.sh
+++ config-script.sh
@@ -2,3 +2,13 @@ echo "Config script"
 
 echo "Linking alacritty config file"
 ln -s ~/dotfiles/.alacritty.yml ~/.alacritty.yml
+
+# checking and symlink nvim folder
+echo "Checking if nvim folder exits"
+if [ -d "~/.config/nvim" ]
+then
+	echo "Folder exits"
+else
+	echo "Folder not exits"
+	ln -s ~/dotfiles/nvim ~/.config/
+fi
