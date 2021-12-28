f8d644529cd104d2365c0193b037a58e8b71f9c7
ThanhTam <tam.le@avironactive.com>
2021-12-28 15:14:49 +0700

    Adding tmux config

--- config-script.sh
+++ config-script.sh
@@ -3,12 +3,25 @@ echo "Config script"
 echo "Linking alacritty config file"
 ln -s ~/dotfiles/.alacritty.yml ~/.alacritty.yml
 
+# Checking and symlink
+[ -d "~/.alacritty.yml" ] && echo "WAO"
+
 # checking and symlink nvim folder
 echo "Checking if nvim folder exits"
 if [ -d "~/.config/nvim" ]
 then
-	echo "Folder exits"
+  echo "Folder exits"
 else
-	echo "Folder not exits"
-	ln -s ~/dotfiles/nvim ~/.config/
+  echo "Folder not exits"
+  ln -s ~/dotfiles/nvim ~/.config/
+fi
+
+# Checking and symlink tmux config file
+echo "Checking tmux config file"
+if [ -e "~/.tmux.conf" ]
+then
+  echo "File exits"
+else
+  echo "File not exits"
+  ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 fi
