813c508f13a32c228d74707339be8dc999ebb56c
ThanhTam <tam.le@avironactive.com>
2022-02-22 11:40:43 +0700

    Add paste from system

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -5,6 +5,20 @@ if not status_ok then
   return
 end
 
+-- Auto install
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    'git', 
+    'clone', 
+    '--depth', 
+    '1', 
+    'https://github.com/wbthomason/packer.nvim', 
+    install_path
+  })
+end
+
 -- Install plugins here
 return packer.startup(function(use)
 
