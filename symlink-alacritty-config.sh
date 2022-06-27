da57610120be30c50c33e1f29942816a0ad4e0f3
Tâm Thanh Lê <ttle@tamlmac.mgm-edv.de>
2022-06-27 13:57:10 +0700

    Add alacritty config shell script

---
+++ symlink-alacritty-config.sh
@@ -0,0 +1,9 @@
+# Checking and symlink tmux config file
+echo "Checking tmux config file"
+if [ -e ~/.alacritty.yml ] || [ -L ~/.alacritty.yml ]
+then
+  echo "Alacritty config exits"
+else
+  echo "File not exits"
+  ln -s ~/dotfiles/.alacritty.yml ~/.alacritty.yml
+fi
