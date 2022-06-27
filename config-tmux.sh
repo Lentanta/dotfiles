0dd3205a336539854a73f4f4c6e9e4e45116f20f
Tâm Thanh Lê <ttle@tamlmac.mgm-edv.de>
2022-06-27 13:46:45 +0700

    Add config shell script for Tmux

---
+++ config-tmux.sh
@@ -0,0 +1,9 @@
+# Checking and symlink tmux config file
+echo "Checking tmux config file"
+if [ -f ~/.tmux.conf ] || [ -L ~/.tmux.conf ]; then
+  echo "File exits"
+
+else
+  echo "File not exits"
+  ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+fi
