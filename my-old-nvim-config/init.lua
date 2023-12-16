7bf899555ef6d5980cfd28ea27a5c5a0fa6740e4
Thanh tam <lentanta@pop-os.localdomain>
2023-12-17 01:44:16 +0700

    Update config for lazyvim

---
+++ my-old-nvim-config/init.lua
@@ -0,0 +1,15 @@
+require("settings")
+require("plugins")
+require("keymaps")
+
+require("plugin-configs.lualine")
+require("plugin-configs.nvim-tree")
+require("plugin-configs.cmp")
+require("plugin-configs.lsp")
+require("plugin-configs.mason")
+require("plugin-configs.null-ls")
+require("plugin-configs.telescope")
+require("plugin-configs.autopairs")
+require("plugin-configs.blankline")
+
+require("plugin-configs.colorshceme")
